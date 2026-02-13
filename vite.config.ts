14bb7c0eaf3f9718ef274bbbdd5a1fc3033b5697
Wellington <welly@wellym2.local>
2026-02-13 10:25:35 -0500

    Github Pages fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/peek-a-bin/",
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ["capstone-wasm"],
